b03d1c354389d99516154bb433331c0bc1da26eb
natye16 <ygopro.log@gmail.com>
2020-12-07 12:51:18 +0000

    second

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema:defalutStringLength(191);
+        Schema::defalutStringLength(191);
     }
 }
